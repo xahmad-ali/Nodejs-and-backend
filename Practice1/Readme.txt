0273198a99e8faf3e53e19b40a91bffa823f1e30
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-09-04 20:19:27 +0500

    abc

--- Practice1/Readme.txt
+++ Practice1/Readme.txt
@@ -1,3 +1,4 @@
 # create simple environment "npm init -y"
 # Install express "npm i express" 
-# 
+# Create index.js
+# Install ejs as view egine
---
+++ Practice1/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json);
+app.use(express.urlencoded({extended: true }));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req, res)=>{
+    res.render("Its Working")
+})
+
+app.listen(3000);
