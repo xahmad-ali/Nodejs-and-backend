b2a22f04054e9b996b2f184c10117caab2a76e12
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-10-08 19:13:23 +0500

    added like,edit and upoadfile functionality in mini project

--- Practice6/index.js
+++ Practice6/index.js
@@ -9,12 +9,31 @@ require('dotenv').config('/env');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer  = require('multer');
+const crypto = require('crypto'); //-> it is prefined javaScript library
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+
+/////////////////
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12,(err,bytes)=>{
+            const fn = bytes.toString("hex") + path.extname(file.originalname)
+            cb(null,fn)
+            console.log("uploaded Successfuly")
+        })
+    }
+});
+
+const upload = multer({ storage: storage })
 
 /////////////
 app.get("/", (req,res)=>{
@@ -77,7 +96,7 @@ app.get("/profile",isloggedIn,async(req, res)=>{
     res.render("profile", {user})
 })
 
-//////////////
+////////////////////
 app.post("/post",isloggedIn,async(req,res)=>{
     let user = await userModel.findOne({email: req.user.email})
     let {content} = req.body
@@ -91,11 +110,47 @@ app.post("/post",isloggedIn,async(req,res)=>{
     res.redirect("/profile")
 })
 
+//////////////
+app.get("/likes/:id", isloggedIn, async (req,res)=>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid)
+    }else{
+        post.likes.pop(req.user.userid)
+    }
+
+    await post.save()
+    res.redirect("/profile");
+})
+
+/////////////////
+app.get("/edit/:id",isloggedIn ,async(req, res)=>{
+    let post = await postModel.findOne({_id: req.params.id})
+    res.render("edit",{post})
+})
+
+app.post("/update/:id",isloggedIn ,async(req, res)=>{
+    // console.log(req.params)
+    let post = await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content})
+
+    res.redirect("/profile")
+})
+
+
+////////////////////
+app.post("/upload",upload.single('image'),isloggedIn,(req,res)=>{
+    console.log(req.file)
+    //console.log(req.body)
+    res.redirect("/profile");
+})
+
 // middleware isloggegin
 function isloggedIn(req,res,next){
     if(req.cookies.token === ""){ res.send("You must be logged in")}
     else{
         const verify= jwt.verify(req.cookies.token, process.env.JWT_TOKEN)
+        //console.log(verify)
         req.user = verify;
         next();
     }
